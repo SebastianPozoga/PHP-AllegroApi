e1d3301d46b587a09ee5c29883dae87d04719201
spozoga <prog255@gmail.com>
2015-01-27 03:16:06 +0100

    version 1.0 - add ApiConfig Helper to translate config array to object

---
+++ AllegroConfig.php
@@ -0,0 +1,22 @@
+<?php
+
+require "AllegroAPIException.php";
+
+class AllegroConfig {
+
+	public $password = null;
+	public $hashPassword = null;
+	public $login = null;
+	public $apikey = null;
+	public $countryCode = null;
+
+
+	function __construct(array $config) {
+		$this->password = isset($config['password'])? $config['password'] : null;
+		$this->hashPassword = isset($config['hashPassword'])? $config['hashPassword'] : null;
+		$this->login = $config['login'];
+		$this->apikey = $config['apikey'];
+		$this->countryCode = $config['countryCode'];
+	}
+
+}
